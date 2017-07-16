b32c505142c7df4c5cf7be892c644deeed10091f
Andrei Varanovich <andrei@Andreis-MacBook.local>
2017-07-16 17:55:21 +0200

    Modified to support size

--- dask/bytes/adl.py
+++ dask/bytes/adl.py
@@ -24,7 +24,7 @@ class AdlFileSystem(AzureDLFileSystem, core.FileSystem):
 
     def _trim_filename(self, fn):
         so = infer_storage_options(fn)
-        return 'adl://' + so.get('host', '') + so['path']
+        return 'https://' + so.get('host', '') + so['path']
 
     def glob(self, path):
         """For a template path, return matching files"""
@@ -41,7 +41,7 @@ class AdlFileSystem(AzureDLFileSystem, core.FileSystem):
         return self.info(path)['ETag']
 
     def size(self, path):
-        adl_path = self._trim_filename(path) + '/Verkooporders.csv'
+        adl_path = self._trim_filename(path)
         return self.info(adl_path)['length']
 
 core._filesystems['adl'] = AdlFileSystem
